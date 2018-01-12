672295bdc887bc9f1571fabe1b20b36a8ea45385
Patryk Szylin <patrykszylin93@gmail.com>
2018-01-12 13:41:31 +0000

    Added personal work scripts for potential review of work

---
+++ Projectiles/Projectile_Rocket.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile_Rocket : Projectile
+{
+    [HideInInspector] public float m_radius;
+
+    Collider otherPlayer;
+
+    public override void Launch()
+    {
+        Rigidbody rbody = Instantiate(m_prefab, m_spawnPos.position, m_spawnPos.rotation) as Rigidbody;
+        if (rbody != null)
+        {
+            rbody.velocity = m_velocity;
+
+            NetworkServer.Spawn(rbody.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        m_startLoc = m_spawnPos.position;
+        //CheckRange = CheckProjectileRange;
+    }
+
+    public override void OnCollisionHit(Collider other)
+    {
+        var explosion = Instantiate(m_impactFX, transform.position, transform.rotation);
+        CameraScript.Instance.CameraShake(); //TODO: Only shake on players near by!!
+        NetworkServer.Spawn(explosion);
+
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_radius);
+
+        foreach (var nearbyObj in colliders)
+        {
+            // Check whether any colliders have destructible on them,
+            // If so, damage them
+            var destructible = nearbyObj.GetComponent<Destructible_ExplosionAndBurning>();
+            if(destructible != null)
+            {
+                destructible.TakeDamage(m_damage, m_owner);
+            }
+
+            // Check if any of the colliders are players
+            // If so, deal damage
+            var player = nearbyObj.GetComponent<PlayerHealth>();
+            if (player != null)
+            {
+                player.Damage(m_damage, m_owner);
+            }
+        }
+
+        Destroy(this.gameObject);
+    }
+
+    
+    [Command]
+    void CmdCheckForCollision()
+    {
+        if(otherPlayer.GetComponent<Player>() != m_owner)
+        {
+            OnCollisionHit(otherPlayer);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        otherPlayer = other;
+        CmdCheckForCollision();
+    }
+}
---
+++ UIManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+
+    #region SINGLETON
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<UIManager>();
+
+                // If the GameObject with GameManager wasn't found, create one.
+                if (_instance == null)
+                    _instance = new GameObject("UIManager").AddComponent<UIManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+    #endregion
+    
+    public List<Text> m_cooldownDisplayTexts;
+    public List<Image> m_darkMasks;
+    public List<Image> m_abilitySprites;
+    public List<Text> m_abilityTooltipObjects;
+
+    // Kill Feed
+    public GameObject killfeedPanelPrefab;
+    public GameObject killfeed;
+    private float killFeedDuration = 4;
+
+    // Damage Text
+    public FloatingText floatingText;
+
+    public void DisplayToolTip(int abilityIndex)
+    {
+        //m_abilityTooltipObjects[abilityIndex].text = m_abilityToolcontent[abilityIndex].text;
+        print("DKSAODASDOS");
+        m_abilityTooltipObjects[abilityIndex].gameObject.SetActive(true);
+        
+    }
+
+    public void HideToolTip(int abilityIndex)
+    {
+        m_abilityTooltipObjects[abilityIndex].gameObject.SetActive(false);
+    }
+
+    // Damage Text
+    public void initDamageText(float text, bool heal, Canvas floatingTextCanvas)
+    {
+        if(floatingText != null)
+        {
+            FloatingText instance = Instantiate(floatingText);
+            instance.transform.SetParent(floatingTextCanvas.transform, false);
+            instance.setText(text, heal);
+        }
+    }
+
+    // Kill Feed
+    public void OnKill(string deadPlayer, string sourcename)
+    {
+        GameObject go = Instantiate(killfeedPanelPrefab, killfeed.transform);
+        go.GetComponent<KillFeedPanel>().Setup(deadPlayer, sourcename);
+        Destroy(go, killFeedDuration);
+    }
+
+}
